bfdf35644e762f576de3bb96f46d92e30be6f674
hsynkmk <huseyinkaymak99@gmail.com>
2024-12-07 21:55:28 +0300

    initial commit

---
+++ PrinciplesOfSOLID/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrinciplesOfSOLID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
