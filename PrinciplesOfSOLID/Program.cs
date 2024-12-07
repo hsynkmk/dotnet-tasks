394aa6b8ce5bfc5353bb17bab1bd2365dfe05d5e
hsynkmk <huseyinkaymak99@gmail.com>
2024-12-07 23:07:42 +0300

    SRP

--- PrinciplesOfSOLID/Program.cs
+++ PrinciplesOfSOLID/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace PrinciplesOfSOLID
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using PrinciplesOfSOLID;
+
+// Single responsibility principle (SRP)
+#if true
+Console.WriteLine("Single responsibility principle (SRP)");
+
+Console.WriteLine("--Without SRP--");
+var wrongOrderManager = new WrongOrderManager();
+wrongOrderManager.ProcessOrder();
+wrongOrderManager.LogOrder();
+wrongOrderManager.NotifyCustomer();
+
+Console.WriteLine("\n--With SRP--");
+var orderProcessor = new OrderProcessor();
+var logger = new Logger();
+var notificationService = new NotificationService();
+
+var orderService = new OrderService(orderProcessor, logger, notificationService);
+orderService.Process();
+#endif
+
+
+Console.ReadLine();
+
