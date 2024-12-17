583fcb95f7839bc6e9d3815220b795ed9a8dea4f
hsynkmk <huseyinkaymak99@gmail.com>
2024-12-17 22:53:39 +0300

    Admin dashboard

--- LibraryManagement/LibraryManagement.Web/Controllers/AccountController.cs
+++ LibraryManagement/LibraryManagement.Web/Controllers/AccountController.cs
@@ -48,14 +48,22 @@ namespace LibraryManagement.Web.Controllers
 
                 if (result.Succeeded)
                 {
-                    if (string.IsNullOrEmpty(loginVM.ReturnUrl))
+                    var user = await _userManager.FindByEmailAsync(loginVM.Email);
+                    if (await _userManager.IsInRoleAsync(user, SD.Role_Admin))
                     {
-                        return RedirectToAction("Index", "Home");
+                        return RedirectToAction("Index", "Dashboard");
                     }
 
                     else
                     {
-                        return LocalRedirect(loginVM.ReturnUrl);
+                        if (string.IsNullOrEmpty(loginVM.ReturnUrl))
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                        else
+                        {
+                            return LocalRedirect(loginVM.ReturnUrl);
+                        }
                     }
                 }
 
